352a41af1c29687623141eab934cd012e352e0f4
Exist Model <sarizava@yandex.ru>
2017-12-04 18:44:58 +0300

    Add popup for specials

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 $(function () {
   // $("body").fadeOut();
 });
+
+
+ $(document).ready(function() {
+        $(".popup__content").magnificPopup({type:"inline", midClick: true});
+
+        $(".specials__item").each(function (i) {
+          $(this).find("a").attr("href","#receipt_" + i);
+          $(this).find(".receipt").attr("id", "receipt_" + i);
+        })
+
+        function slowScroll(id) {
+          var offset = 0;
+          $("html, body").animate ({
+            scrollTop: $(id).offset ().top - offset
+          }, 1000);
+          return false;
+        };
+      });
